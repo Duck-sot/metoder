c368d7d951ca556c2d6a68cb68bbc14ad775b8d2
Duck-sot <raja07001@utb.vaxjo.se>
2024-09-25 11:18:56 +0200

    jag gjorde del 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+void Main(){
+    double radie = 2.4;
+    Console.WriteLine("cirkelens omcrets: " +Omkrets(radie));
+    Console.WriteLine("Cirklens area: " + Area(radie));
+}
+double Area(double r) {
+    double area = Math.Pow(r, 2)*Math.PI;
+    return area;
+}
+double Omkrets(double r){
+    double om = r+r*Math.PI;
+    return om;
+}
+
+Main();*/
+public partial class Form1 : Form{
+    public Form1
+}
